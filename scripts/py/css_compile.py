9f57f23aec61daf0af5b242480c80143eaa2494f
narek4241 <narekghazaryan02@gmail.com>
2020-02-10 22:27:45 +0400

    updated py/css_compile.py

--- scripts/py/css_compile.py
+++ scripts/py/css_compile.py
@@ -6,62 +6,61 @@ print("Will only compile")
 filename = '../..' '/app.min.css'
 new_file = open(filename, 'w')
 
-# modules js
+# modules css
 
-file1 = open('../js/modules' +'/header.js', 'r')
+file1 =open('../../styles' +'/animate.css', 'r')
 new_file.write(file1.read())
-print("1 js is written")
+print("1 css is written")
 
-file2 = open('../js/modules' +'/timer.js', 'r')
+file2 =open('../../styles' +'/answers.css', 'r')
 new_file.write(file2.read())
-print("2 js is written")
+print("2 css is written")
 
-file3 = open('../js/modules' +'/dialog.js', 'r')
+file3 =open('../../styles' +'/back-to-top.css', 'r')
 new_file.write(file3.read())
-print("3 js is written")
+print("3 css is written")
 
-file4 = open('../js/modules' +'/answers.js', 'r')
+file4 =open('../../styles' +'/footer.css', 'r')
 new_file.write(file4.read())
-print("4 js is written")
+print("4 css is written")
 
-file5 = open('../js/modules' +'/answers-input-method.js', 'r')
+file5 =open('../../styles' +'/header.css', 'r')
 new_file.write(file5.read())
-print("5 js is written")
+print("5 css is written")
 
-file6 = open('../js/modules' +'/answers-status-table.js', 'r')
+file6 =open('../../styles' +'/result-table.css', 'r')
 new_file.write(file6.read())
-print("6 js is written")
+print("6 css is written")
 
-file7 = open('../js/modules' +'/back-to-top.js', 'r')
+file7 =open('../../styles' +'/select.css', 'r')
 new_file.write(file7.read())
-print("7 js is written")
+print("7 css is written")
 
-file8 = open('../js/modules' +'/check-answers.js', 'r')
+file8 =open('../../styles' +'/side-nav.css', 'r')
 new_file.write(file8.read())
-print("8 js is written")
+print("8 css is written")
 
-file9 = open('../js/modules' +'/result-table.js', 'r')
+file9 =open('../../styles' +'/style.css', 'r')
 new_file.write(file9.read())
-print("9 js is written")
+print("9 css is written")
 
-file10 = open('../js/modules' +'/select.js', 'r')
+file10 =open('../../styles' +'/take-care.css', 'r')
 new_file.write(file10.read())
-print("10 js is written")
+print("10 css is written")
 
-file11 = open('../js/modules' +'/task-iframe.js', 'r')
+file11 =open('../../styles' +'/task-iframe.css', 'r')
 new_file.write(file11.read())
-print("11 js is written")
+print("11 css is written")
 
-file12 = open('../js/modules' +'/side-nav.js', 'r')
+file12 =open('../../styles' +'/timer.css', 'r')
 new_file.write(file12.read())
-print("12 js is written")
+print("12 css is written")
 
-file13 = open('../js/modules' +'/scroll.js', 'r')
+file13 =open('../../styles' +'/toolbar.css', 'r')
 new_file.write(file13.read())
-print("13 js is written")
+print("13 css is written")
 
 new_file.close()
-
 
 
 
